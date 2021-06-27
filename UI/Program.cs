41f2fe72be2f75e6e91fb3af4651be6c3a8ceadb
HP <HP@DESKTOP-U692M88>
2021-06-28 03:54:34 +0800

    Add project files.

---
+++ UI/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BusinessLayer;
+using DataLayer;
+
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//email: "employee@leaves.company";
+			//password: "password"
+
+			int count = 0;
+			while (count < 5)
+			{
+				if (count == 4)
+				{
+					Console.WriteLine("Maximum tries exceeded!");
+					return;
+				}
+
+				//SqlData.Connect();
+
+				Console.WriteLine("Enter email for Login: ");
+				string InputEmail = Console.ReadLine();
+				Console.WriteLine("Enter Password: ");
+				string InputPassword = Console.ReadLine();
+
+				if (InputEmail == "employee@leaves.company" && InputPassword == "password")
+				{
+					Console.WriteLine("Login Succesful");
+					Menu.selection();
+					break;
+				}
+
+
+				else
+				{
+					count++;
+				}
+
+
+			}
+		}
+    }
+}
